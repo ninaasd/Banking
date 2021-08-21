dc276f8cac8ecf316f327a40ac506884d268e35f
ninaasd <ninu7868@gmail.com>
2021-08-21 12:42:36 +0530

    initial commit

---
+++ src/home.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {  Navbar, Nav, Container, Button,Row , Col} from 'react-bootstrap';
+import logo from './images/logo.jpg'
+import bg from './images/bg.jpg'
+import bg1 from './images/bg1.jepg'
+class Home extends Component {
+    render() {
+        return (
+            <div >
+                <>
+                
+
+  
+  <div  className = "content" style={{ 
+    backgroundColor: "#d6f2ff",
+      paddingBottom: "4rem",
+      backgroundImage: 'url('+bg1+')'
+
+    }}>
+       <Container>
+           <Row style={{padding: "200px"}}>
+               <Col>
+               <Button  style={{width: "180px"}} variant="outline-success" href="/users">Users List</Button>{' '}
+               </Col>
+               <Col>
+               <Button style={{width: "180px"}} variant="outline-danger" href="/transfer">Transfer</Button>{' '}
+               </Col>
+               <Col>
+               <Button style={{width: "180px"}} variant="outline-info" href="/transactions">Transaction History</Button>{' '}
+               </Col>
+           </Row>
+           </Container> 
+        
+        
+        
+        
+    </div>
+</>
+            </div>
+        )
+    }
+}
+export default Home
+
